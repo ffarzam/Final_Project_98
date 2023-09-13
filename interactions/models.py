9e611011d65547509dbd87549ac73ea350cffe68
Farzam <ffarzam_1992@yahoo.com>
2023-09-13 21:20:21 +0330

    chore: add subscription model

--- interactions/models.py
+++ interactions/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
-# from feeder.models import Channel
+from feeder.models import Channel
 
 
 class Like(models.Model):
@@ -20,5 +20,12 @@ class Comment(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='subscriptions')
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE, related_name='subscriptions')
+    subscribe_at = models.DateTimeField(auto_now_add=True)
+
 
 
