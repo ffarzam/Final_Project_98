ed1fd5b701565be18c35c74605f632f2cf89cebc
Farzam <ffarzam_1992@yahoo.com>
2023-09-13 21:02:52 +0330

    chore: add channel_parser_one function

--- feeder/parsers.py
+++ feeder/parsers.py
@@ -16,3 +16,56 @@ def get_info(xml_link):
     return channel, namespaces
 
 
+def channel_parser_one(xml_link):
+    channel, namespaces = get_info(xml_link)
+
+    title = channel.find("title").text if channel.find("title") is not None else None
+
+    subtitle = channel.find("itunes:subtitle", namespaces=namespaces).text \
+        if channel.find("itunes:subtitle", namespaces=namespaces) is not None \
+        else None
+
+    last_update = datetime.strptime(channel.find("pubDate").text, "%a, %d %b %Y %H:%M:%S %z") \
+        if channel.find("pubDate") is not None \
+        else None
+
+    description = channel.find("description").text if channel.find("description") is not None else None
+
+    language = channel.find("language").text if channel.find("language") is not None else None
+
+    image_file_url = channel.find("itunes:image", namespaces=namespaces).attrib.get("href") \
+        if channel.find("itunes:image", namespaces=namespaces) is not None \
+        else None
+    if image_file_url is None:
+        image_file_url = channel.find("image/url").text \
+            if channel.find("image/url") is not None \
+            else None
+
+    author = channel.find("itunes:author", namespaces=namespaces).text \
+        if channel.find("itunes:author", namespaces=namespaces) is not None \
+        else None
+
+    owner = channel.find("itunes:owner/itunes:name", namespaces=namespaces).text \
+        if channel.find("itunes:owner/itunes:name", namespaces=namespaces) is not None \
+        else None
+
+    owner_email = channel.find("itunes:owner/itunes:email", namespaces=namespaces).text \
+        if channel.find("itunes:owner/itunes:email", namespaces=namespaces) is not None \
+        else None
+
+    channel_info = {
+        "title": title,
+        "subtitle": subtitle,
+        "last_update": last_update,
+        "description": description,
+        "language": language,
+        "author": author,
+        "owner": owner,
+        "owner_email": owner_email,
+        "image_file_url": image_file_url,
+    }
+
+    return channel_info
+
+
+
