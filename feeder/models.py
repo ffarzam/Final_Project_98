49bd80714bd42dc06dd7a992fcac23651592282a
Farzam <ffarzam_1992@yahoo.com>
2023-09-13 20:53:54 +0330

    chore: add xml link  model

--- feeder/models.py
+++ feeder/models.py
@@ -13,3 +13,18 @@ class Type(models.Model):
         return f"{self.name}"
 
 
+class XmlLink(models.Model):
+    xml_link = models.URLField(unique=True)
+    channel_parser = models.CharField(max_length=50)
+    items_parser = models.CharField(max_length=50)
+    rss_type = models.ForeignKey(Type, on_delete=models.PROTECT, null=True, blank=True)
+    categories = models.ManyToManyField(Category, blank=True)
+
+    def __str__(self):
+        try:
+            name = Channel.objects.get(xml_link=self).title
+            return f'{name} || {self.xml_link}'
+        except:
+            return f'{self.xml_link}'
+
+
