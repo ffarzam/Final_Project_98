b1814bd48602d3a6e4dcfb54a8aac712f996cb6a
Farzam <ffarzam_1992@yahoo.com>
2023-09-14 21:44:31 +0330

    chore: add channel list url

--- feeder/urls.py
+++ feeder/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('create/', views.CreateChannelAndItems.as_view(), name='create'),
     path('update/', views.UpdateChannelAndItems.as_view(), name='update'),
+    path('channel_list/', views.ChannelList.as_view(), name='channel_list'),
 ]
-
-
