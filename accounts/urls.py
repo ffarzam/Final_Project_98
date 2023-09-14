8287d2319ede5b5f3922d0c6f8207bad11f65478
Farzam <ffarzam_1992@yahoo.com>
2023-09-14 16:49:22 +0330

    refactor: remove status view

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('status/', views.Status.as_view(), name='status'),
     path('login/', views.UserLogin.as_view(), name='login'),
     path('login/refresh/', views.RefreshToken.as_view(), name='token_refresh'),
     path('register/', views.UserRegister.as_view(), name='register'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('active_login/', views.CheckAllActiveLogin.as_view(), name='active_login'),
 
 ]
 
