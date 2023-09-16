e308336ea42b4e0e85f9ce47e8b221b56cde8583
Farzam <ffarzam_1992@yahoo.com>
2023-09-17 01:11:49 +0330

    chore: add logout all urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('login/refresh/', views.RefreshToken.as_view(), name='token_refresh'),
     path('register/', views.UserRegister.as_view(), name='register'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('logout_all/', views.LogoutAll.as_view(), name='logout_all'),
     path('active_login/', views.CheckAllActiveLogin.as_view(), name='active_login'),
     path('selected_logout/', views.SelectedLogout.as_view(), name='selected_logout'),
     path('profile/', views.ShowProfile.as_view(), name='profile'),
