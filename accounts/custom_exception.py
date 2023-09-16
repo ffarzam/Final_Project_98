a1aa5fcf06679e9e320ec88b16595665823f16af
Farzam <ffarzam_1992@yahoo.com>
2023-09-16 11:55:44 +0330

    chore: add expired refresh token error exceptions

--- accounts/custom_exception.py
+++ accounts/custom_exception.py
@@ -8,3 +8,10 @@ class ExpiredAccessTokenError(APIException):
     default_detail = _('Access Token Has Been Expired')
     default_code = 'expired_access_token'
 
+
+class ExpiredRefreshTokenError(APIException):
+    status_code = status.HTTP_403_FORBIDDEN
+    default_detail = _('Refresh Token Has Been Expired, Please Login Again')
+    default_code = 'expired_refresh_token'
+
+
