e4df77458da09f629d5c8f2f755dcf0c9fc2b610
Farzam <ffarzam_1992@yahoo.com>
2023-09-17 08:41:21 +0330

    chore: add get item url

--- feeder/urls.py
+++ feeder/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('channel_list/', views.ChannelList.as_view(), name='channel_list'),
     path('items_list/<int:channel_id>', views.ItemsList.as_view(), name='items_list'),
     path('get_channel/<int:pk>', views.GetChannel.as_view(), name='get_channel'),
+    path('get_item/<int:channel_id>/<int:item_id>', views.GetItem.as_view(), name='get_item'),
 ]
