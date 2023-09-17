ea9bc05f334bac53bd59abe28b3ac55e6f61037d
Farzam <ffarzam_1992@yahoo.com>
2023-09-17 08:40:51 +0330

    chore: add get channel url

--- feeder/urls.py
+++ feeder/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('update/', views.UpdateChannelAndItems.as_view(), name='update'),
     path('channel_list/', views.ChannelList.as_view(), name='channel_list'),
     path('items_list/<int:channel_id>', views.ItemsList.as_view(), name='items_list'),
+    path('get_channel/<int:pk>', views.GetChannel.as_view(), name='get_channel'),
 ]
