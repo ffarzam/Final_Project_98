c09d930d8b87d18134513caa749ce4557220c53f
Farzam <ffarzam_1992@yahoo.com>
2023-09-18 11:59:24 +0330

    chore: add str method

--- feeder/models.py
+++ feeder/models.py
@@ -5,9 +5,15 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Type(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
 
     def __str__(self):
         return f"{self.name}"
