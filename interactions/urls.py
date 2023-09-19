cd9638154fa5cae3bf1ce981ef91a637fc3584c8
Farzam <ffarzam_1992@yahoo.com>
2023-09-19 21:26:58 +0330

    chore: add recommendation url

--- interactions/urls.py
+++ interactions/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('like/', views.LikeView.as_view(), name='like'),
+    path('recommendation/', views.RecommendationView.as_view(), name='recommendation'),
 
 ]
