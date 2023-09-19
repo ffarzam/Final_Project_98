d70ffd0eb13bfcd531c457775dd39e640b7b3a48
Farzam <ffarzam_1992@yahoo.com>
2023-09-19 23:43:02 +0330

    refactor: add password validatore to user register serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,30 +1,32 @@
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 from .models import CustomUser
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'password', 'password2')
-        extra_kwargs = {
-            'password': {'write_only': True},
-        }
 
     def create(self, validated_data):
         del validated_data['password2']
         return CustomUser.objects.create_user(**validated_data)
 
-    @staticmethod
-    def validate_username(value):
-        if len(value) <= 8:
-            raise serializers.ValidationError('Username must be more than 8 character long')
+    def validate_username(self, value):
+        if len(value) < 6 or len(value) > 15:
+            raise ValidationError('Username must be between 6 and 15 characters long')
         return value
 
     def validate(self, data):
         if data['password'] != data['password2']:
             raise serializers.ValidationError("Passwords don't match")
+        if data['email'] == data['username']:
+            raise serializers.ValidationError("Email and username can't be same")
+
         return data
 
 
@@ -37,4 +39,4 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         # fields = '__all__' #???
-        exclude = ["password", "groups", "user_permissions"] #???
+        exclude = ["password", "groups", "user_permissions"]  # ???
