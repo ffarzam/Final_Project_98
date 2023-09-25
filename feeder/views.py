e9bd720391f505e49a3c06bfe4eb254003e6771c
Farzam <ffarzam_1992@yahoo.com>
2023-09-25 19:42:54 +0330

    refactor: update feedr views

--- feeder/views.py
+++ feeder/views.py
@@ -1,54 +1,25 @@
+from celery import shared_task
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 from rest_framework.generics import ListAPIView, GenericAPIView, RetrieveAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
 from django.db.models import Q, F
-from django.db import transaction, IntegrityError
 
-from .models import XmlLink, Channel, SearchCount, Episode, News
-from .parsers import channel_parser_mapper, items_parser_mapper, item_model_mapper
+from .models import Channel, SearchCount, Episode, News
+from .parsers import item_model_mapper
 from .serializer import ChannelSerializer, EpisodeSerializer, NewsSerializer
 from .utils import item_serializer_mapper, ChannelPagination, ItemsPagination
 
 from accounts.authentication import AccessTokenAuthentication
 from Permissions import IsSuperuser
 
+from . import tasks
+
 
 # Create your views here.
-
-
-class CreateChannelAndItems(APIView):
-    authentication_classes = (AccessTokenAuthentication,)
-    permission_classes = (IsSuperuser,)
-
-    def post(self, request):
-
-        xml_link = request.data['xml_link']
-        xml_link_qs = XmlLink.objects.filter(xml_link=xml_link)
-        if xml_link_qs.exists():
-            xml_link_obj = xml_link_qs.get()
-            channel_qs = Channel.objects.filter(xml_link=xml_link_obj)
-            if not channel_qs.exists():
-
-                channel_parser = channel_parser_mapper(xml_link_obj.channel_parser)
-                items_parser = items_parser_mapper(xml_link_obj.items_parser)
-                channel_info = channel_parser(xml_link_obj.xml_link)
-                ItemClass = item_model_mapper(xml_link_obj.rss_type.name)
-                items_info = items_parser(xml_link_obj.xml_link)
-                try:
-                    with transaction.atomic():
-                        channel = Channel.objects.create(xml_link=xml_link_obj, **channel_info)
-                        items = (ItemClass(**item, channel=channel) for item in items_info)
-                        ItemClass.objects.bulk_create(items)
-                except IntegrityError as e:
-                    return Response({"Message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
-                return Response({"Message": "Channel and the Items Was Created"}, status=status.HTTP_201_CREATED)
-
-            return Response({"Message": "Channel Already Exists"}, status=status.HTTP_400_BAD_REQUEST)
-
-        return Response({"Message": "No Link Was Found"}, status=status.HTTP_404_NOT_FOUND)
 
 
 class UpdateChannelAndItems(APIView):
@@ -57,47 +28,20 @@ class UpdateChannelAndItems(APIView):
 
     def post(self, request):
 
-        xml_link = request.data['xml_link']
-        xml_link_qs = XmlLink.objects.filter(xml_link=xml_link)
-        if xml_link_qs.exists():
-            xml_link_obj = xml_link_qs.get()
-            channel_qs = Channel.objects.filter(xml_link=xml_link_obj)
-            if channel_qs.exists():
-                channel = channel_qs.get()
-                channel_parser = channel_parser_mapper(xml_link_obj.channel_parser)
-                channel_info = channel_parser(xml_link_obj.xml_link)
+        xml_link = request.data.get('xml_link')
+        if xml_link:
+            tasks.update_single_rss.delay(xml_link)
+        else:
+            tasks.update_all_rss.delay()
 
-                if channel.last_update != channel_info.get("last_update"):
-                    channel.last_update = channel_info.get("last_update")
-                    items_parser = items_parser_mapper(xml_link_obj.items_parser)
-                    items_info = items_parser(xml_link_obj.xml_link)
-                    ItemClass = item_model_mapper(xml_link_obj.rss_type.name)
-                    try:
-                        with transaction.atomic():
-                            channel.save()
-                            items = (
-                                ItemClass(**item, channel=channel)
-                                for item in items_info
-                                if not ItemClass.objects.filter(guid=item.get("guid")).exists()
-                            )
-                            ItemClass.objects.bulk_create(items)
-                    except IntegrityError as e:
-                        return Response({"Message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
-                    return Response({"Message": "Channel was Updated"}, status=status.HTTP_201_CREATED)
-
-                return Response({"Message": "Channel is Already Updated"}, status=status.HTTP_400_BAD_REQUEST)
-
-            return Response({"Message": "Channel Doesn't Exist"}, status=status.HTTP_404_NOT_FOUND)
-
-        return Response({"Message": "No Link Was Found"}, status=status.HTTP_404_NOT_FOUND)
+        return Response({"Update will be done soon"}, status=status.HTTP_200_OK)
 
 
 class ChannelList(ListAPIView):
-    queryset = Channel.objects.prefetch_related("searchcount_set").annotate(
-        search_count=F("searchcount__count")).order_by(F("search_count").desc(nulls_last=True))
     serializer_class = ChannelSerializer
     pagination_class = ChannelPagination
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['title']
 
     def get_queryset(self):
 
@@ -106,7 +50,8 @@ class ChannelList(ListAPIView):
             queryset = Channel.objects.select_related("xml_link").prefetch_related("xml_link__categories").filter(
                 xml_link__categories__name=filter_query)
         else:
-            queryset = self.queryset
+            queryset = Channel.objects.prefetch_related("searchcount_set").annotate(
+                search_count=F("searchcount__count")).order_by(F("search_count").desc(nulls_last=True))
         return queryset
 
 
