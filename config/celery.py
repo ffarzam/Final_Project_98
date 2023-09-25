6e547b5379dcfd66da94ad3e1a767f15a00b1474
Farzam <ffarzam_1992@yahoo.com>
2023-09-25 19:40:00 +0330

    chore: add celery config

---
+++ config/celery.py
@@ -0,0 +1,17 @@
+import os
+from celery import Celery
+from celery.signals import setup_logging
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+app = Celery('config')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+
+@setup_logging.connect
+def config_loggers(*args, **kwargs):
+    from logging.config import dictConfig
+    from django.conf import settings
+    dictConfig(settings.LOGGING)
+
+
+app.autodiscover_tasks()
