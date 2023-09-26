3a84eec96327795f69357e62f07b38638078a351
Farzam <ffarzam_1992@yahoo.com>
2023-09-26 16:29:41 +0330

    chore: add email setting

--- config/settings.py
+++ config/settings.py
@@ -255,3 +255,12 @@ LOGGING = {
 }
 
 PASSWORD_RESET_TIMEOUT = 20 * 60  # 20 min
+
+
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
+EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND")
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
