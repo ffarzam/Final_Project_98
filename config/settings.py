0829f64fb4bb36185171dcff34fcffefe63754c2
Farzam <ffarzam_1992@yahoo.com>
2023-10-04 15:09:41 +0330

    refactor: remove hit count middleware

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'accounts.custom_middleware.HitCountMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
