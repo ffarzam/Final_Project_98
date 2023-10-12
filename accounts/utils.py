81ede31dbcdb042cd8a66f5440dcfb787bc3c973
unknown <ffarzam_1992@yahoo.com>
2023-10-12 20:56:41 +0330

    refactor: update send email

--- accounts/utils.py
+++ accounts/utils.py
@@ -60,6 +60,6 @@ def send_email(data):
     email = EmailMessage(
         subject=data['email_subject'],
         body=data["email_body"],
-        to=[data["to_email"]]
+        to=data["to_email"]
     )
     email.send()
