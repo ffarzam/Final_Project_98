83b53d3f34551cec98d69462370bf3be9327b884
unknown <ffarzam_1992@yahoo.com>
2023-10-12 20:55:44 +0330

    style: remove extra comma

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -1,6 +1,6 @@
 import logging
 
-from celery import shared_task,
+from celery import shared_task
 
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.urls import reverse
