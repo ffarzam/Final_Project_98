25a14189d03f9c3507e2628cff65a0a2de7595d9
unknown <ffarzam_1992@yahoo.com>
2023-10-21 22:08:04 +0330

    refactore: update account views

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,4 @@
 from django.contrib.sites.shortcuts import get_current_site
-from django.utils.encoding import force_str
-from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth import authenticate
 from django.core.cache import caches
 from django.utils.translation import gettext_lazy as _
@@ -13,12 +11,13 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from .models import CustomUser
 from .serializers import UserRegisterSerializer, UserLoginSerializer, ProfileSerializer, ChangePasswordSerializer, \
-    UpdateUserSerializer, PasswordResetSerializer, SetNewPasswordSerializer, AccountVerificationSerializer
+    UpdateUserSerializer, PasswordResetSerializer, SetNewPasswordSerializer, AccountVerificationSerializer, \
+    VerifyAccountResetSerializer
 from .authentication import AccessTokenAuthentication, RefreshTokenAuthentication
 from .tasks import send_link
-from .utils import generate_refresh_token, generate_access_token, jti_maker, cache_key_setter, cache_value_setter, \
-    cache_key_parser, get_client_ip_address
-from .publisher import publish
+from .utils import cache_key_parser, publisher, set_token
+
+from config.custom_trottle import ResetPasswordRateThrottle, VerifyAccountRateThrottle, SetPasswordRateThrottle
 
 
 # Create your views here.
@@ -29,44 +28,63 @@ class UserRegister(APIView):
     serializer_class = UserRegisterSerializer
 
     def post(self, request):
-        ser_data = self.serializer_class(data=request.POST)
-        if ser_data.is_valid():
-            ser_data.create(ser_data.validated_data)
-            username = ser_data.validated_data["username"]
-            app_name = request.resolver_match.app_name
-            url_name = request.resolver_match.url_name
-            unique_id = request.unique_id
-            user = CustomUser.objects.get(username=username)
-            request.user = user
-            current_site = get_current_site(request).domain
+        serializer = self.serializer_class(data=request.POST)
+        serializer.is_valid(raise_exception=True)
+        serializer.create(serializer.validated_data)
+        username = serializer.validated_data["username"]
+        user = CustomUser.objects.get(username=username)
+        request.user = user
+        current_site = get_current_site(request).domain
 
-            send_link.delay(current_site, user.id, app_name, url_name, unique_id)
+        send_link.delay(current_site, user.id, request.resolver_match.app_name, request.resolver_match.url_name,
+                        request.unique_id)
 
-            return Response({'message': _("Registered successfully, activation link was sent to your email"),
-                             "data": ser_data.data},
-                            status=status.HTTP_201_CREATED)
-        return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
+        message = f"User with {user.username} successfully registered"
+        routing_key = "register"
+        publisher(request, user, message, routing_key)
+
+        return Response({'message': _("Registered successfully, activation link was sent to your email"),
+                         "data": serializer.data},
+                        status=status.HTTP_201_CREATED)
+
+
+class VerifyAccountResetView(APIView):
+    throttle_classes = [VerifyAccountRateThrottle]
+    serializer_class = VerifyAccountResetSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        request.user = user
+        if user.is_active:
+            return Response({"message": _("Your account has already activated")}, status=status.HTTP_200_OK)
+        current_site = get_current_site(request).domain
+
+        send_link.delay(current_site, user.id, request.resolver_match.app_name, request.resolver_match.url_name,
+                        request.unique_id)
+        return Response({"message": _("A link Was Sent To You To Activate Your Account")}, status=status.HTTP_200_OK)
 
 
 class VerifyAccount(APIView):
+    throttle_classes = [VerifyAccountRateThrottle]
     serializer_class = AccountVerificationSerializer
 
     def get(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data, context={"kwargs": kwargs})
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data
+        request.user = user
+        if user.is_active:
+            return Response({"message": _("Your account has already activated")}, status=status.HTTP_200_OK)
+
         user.is_active = True
         user.save()
 
-        info = {
-            "unique_id": request.unique_id,
-            "username": user.username,
-            "message": f"User with {user.username} successfully registered",
-            "routing_key": "register",
-            "user_agent": request.META.get('HTTP_USER_AGENT', 'UNKNOWN'),
-            "ip": get_client_ip_address(request) or " "
-        }
-        publish(info)
+        message = f"Account for {user.username} has been successfully activated"
+        routing_key = "activate"
+        publisher(request, user, message, routing_key)
+
         return Response({"success": _("Your account has been activated successfully")}, status=status.HTTP_200_OK)
 
 
@@ -82,29 +100,19 @@ class UserLogin(APIView):
         user = authenticate(request, user_identifier=user_identifier, password=password)
         if user is None:
             return Response({'message': _('Invalid Credentials')}, status=status.HTTP_400_BAD_REQUEST)
+        if not user.is_active:
+            return Response({'message': _("User hasn't activated his/her account yet")},
+                            status=455)
 
-        jti = jti_maker()
-        access_token = generate_access_token(user.id, jti)
-        refresh_token = generate_refresh_token(user.id, jti)
-        key = cache_key_setter(user.id, jti)
-        value = cache_value_setter(request)
-        caches['auth'].set(key, value)
+        access_token, refresh_token = set_token(request, user, caches)
+        data = {"access": access_token, "refresh": refresh_token}
 
-        data = {
-            "access": access_token,
-            "refresh": refresh_token,
-        }
-        info = {
-            "unique_id": request.unique_id,
-            "username": user.username,
-            "message": f"New Login Record With {request.META.get('HTTP_USER_AGENT', 'UNKNOWN')}",
-            "routing_key": "login",
-            "user_agent": request.META.get('HTTP_USER_AGENT', 'UNKNOWN'),
-            "ip": get_client_ip_address(request) or " "
+        message = f"New Login Record for {user.username} With {request.META.get('HTTP_USER_AGENT', 'UNKNOWN')}"
+        routing_key = "login"
+        publisher(request, user, message, routing_key)
 
-        }
-        publish(info)
         request.user = user
+
         return Response({"message": _("Logged in successfully"), "data": data}, status=status.HTTP_201_CREATED)
 
 
@@ -119,27 +127,13 @@ class RefreshToken(APIView):
         jti = payload["jti"]
         caches['auth'].delete(f'user_{user.id} || {jti}')
 
-        jti = jti_maker()
-        access_token = generate_access_token(user.id, jti)
-        refresh_token = generate_refresh_token(user.id, jti)
+        access_token, refresh_token = set_token(request, user, caches)
+        data = {"access": access_token, "refresh": refresh_token}
 
-        key = cache_key_setter(user.id, jti)
-        value = cache_value_setter(request)
-        caches['auth'].set(key, value)
-        info = {
-            "unique_id": request.unique_id,
-            "username": user.username,
-            "message": f"New Login Record With {request.META.get('HTTP_USER_AGENT', 'UNKNOWN')}",
-            "routing_key": "refresh",
-            "user_agent": request.META.get('HTTP_USER_AGENT', 'UNKNOWN'),
-            "ip": get_client_ip_address(request) or " "
-        }
-        publish(info)
+        message = f"New Login Record With {request.META.get('HTTP_USER_AGENT', 'UNKNOWN')}"
+        routing_key = "refresh"
+        publisher(request, user, message, routing_key)
 
-        data = {
-            "access": access_token,
-            "refresh": refresh_token
-        }
         return Response(data, status=status.HTTP_201_CREATED)
 
 
@@ -153,17 +147,10 @@ class LogoutView(APIView):
             user = request.user
             jti = payload["jti"]
             caches['auth'].delete(f'user_{user.id} || {jti}')
-            info = {
-                "unique_id": request.unique_id,
-                "username": user.username,
-                "message": f"Logout Record With {request.META.get('HTTP_USER_AGENT', 'UNKNOWN')}",
-                "routing_key": "logout",
-                "user_agent": request.META.get('HTTP_USER_AGENT', 'UNKNOWN'),
-                "ip": get_client_ip_address(request) or " "
-            }
-            publish(info)
-
-            return Response({"message": True}, status=status.HTTP_200_OK)
+            message = f"Logout Record With {request.META.get('HTTP_USER_AGENT', 'UNKNOWN')}"
+            routing_key = "logout"
+            publisher(request, user, message, routing_key)
+            return Response({"message": _("Logged out successfully")}, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -194,16 +181,9 @@ class LogoutAll(APIView):
     def get(self, request):
         user = request.user
         caches['auth'].delete_many(caches['auth'].keys(f'user_{user.id} || *'))
-        info = {
-            "unique_id": request.unique_id,
-            "username": user.username,
-            "message": f"Logout-all Record With {request.META.get('HTTP_USER_AGENT', 'UNKNOWN')}",
-            "routing_key": "logout_all",
-            "user_agent": request.META.get('HTTP_USER_AGENT', 'UNKNOWN'),
-            "ip": get_client_ip_address(request) or " "
-        }
-        publish(info)
-
+        message = f"Logout-all Record With {request.META.get('HTTP_USER_AGENT', 'UNKNOWN')}"
+        routing_key = "logout_all"
+        publisher(request, user, message, routing_key)
         return Response({"message": _("All accounts logged out")}, status=status.HTTP_200_OK)
 
 
@@ -216,15 +196,10 @@ class SelectedLogout(APIView):
         jti = request.data.get("jti")
         user_agent = caches['auth'].get(f'user_{user.id} || {jti}')
         caches['auth'].delete(f'user_{user.id} || {jti}')
-        info = {
-            "unique_id": request.unique_id,
-            "username": user.username,
-            "message": f"Logout Record With {request.META.get('HTTP_USER_AGENT', 'UNKNOWN')} for session {user_agent}",
-            "routing_key": "selected_logout",
-            "user_agent": request.META.get('HTTP_USER_AGENT', 'UNKNOWN'),
-            "ip": get_client_ip_address(request) or " "
-        }
-        publish(info)
+
+        message = f"Logout Record With {request.META.get('HTTP_USER_AGENT', 'UNKNOWN')} for session {user_agent}"
+        routing_key = "selected_logout"
+        publisher(request, user, message, routing_key)
 
         return Response({"message": _("Chosen account was successfully logged out")}, status=status.HTTP_200_OK)
 
@@ -265,41 +240,32 @@ class UpdateProfileView(UpdateAPIView):
 
 
 class PasswordResetRequestView(GenericAPIView):
+    throttle_classes = [ResetPasswordRateThrottle]
     serializer_class = PasswordResetSerializer
 
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-        email = serializer.validated_data['email']
-
-        user = CustomUser.objects.filter(email=email)
-        if not user.exists():
-            return Response({'message': _("This email doesn't exist")}, status=status.HTTP_400_BAD_REQUEST)
-        user = user.get()
+        user = serializer.validated_data
+        request.user = user
         current_site = get_current_site(request).domain
-
-        app_name = request.resolver_match.app_name
-        url_name = request.resolver_match.url_name
-        unique_id = request.unique_id
-
-        send_link.delay(current_site, user.id, app_name, url_name, unique_id)
+        send_link.delay(current_site, user.id, request.resolver_match.app_name, request.resolver_match.url_name,
+                        request.unique_id)
         request.user = user
 
         return Response({"message": _("A link Was Sent To You To Reset Your Password")}, status=status.HTTP_200_OK)
 
 
 class SetNewPasswordView(GenericAPIView):
+    throttle_classes = [SetPasswordRateThrottle]
     serializer_class = SetNewPasswordSerializer
 
     def patch(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data, context={"kwargs": kwargs})
         serializer.is_valid(raise_exception=True)
 
-        uidb64 = kwargs["uidb64"]
-
-        user_id = force_str(urlsafe_base64_decode(uidb64))
-        user = CustomUser.objects.get(id=user_id)
-        password = serializer.validated_data['password']
+        user, password = serializer.validated_data
         user.set_password(password)
         user.save()
+        request.user = user
         return Response({"success": _("Password Reset Done")}, status=status.HTTP_200_OK)
